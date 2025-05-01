d11d9c69ddc65a65b110504654afcd76c8ef5061
21v1u5 <4raujopenh4@gmail.com>
2025-05-01 16:37:37 -0300

    melhorando o js da page

--- script.js
+++ script.js
@@ -3,3 +3,11 @@ const singupToggle = document.getElementById('singupToggle');
 const loginForm = document.getElementById('loginForm');
 const singupForm = document.getElementById('singupForm');
 const slider = document.querySelector('.toggle .slider');
+
+loginToggle.addEventListener('click', () => {
+    loginToggle.classList.add('active');
+    singupToggle.classList.remove('active');
+    loginForm.classList.add('active');
+    singupForm.classList.remove('active');
+    slider.style.transform = 'translatex(0)';
+});
