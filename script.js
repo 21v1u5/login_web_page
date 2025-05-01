dbc89d2c02163266e7d9a3112b0e5c58ccd08cee
21v1u5 <4raujopenh4@gmail.com>
2025-05-01 16:32:56 -0300

    iniciando o script em js para a page

---
+++ script.js
@@ -0,0 +1,5 @@
+const loginToggle = document.getElementById('loginToggle');
+const singupToggle = document.getElementById('singupToggle');
+const loginForm = document.getElementById('loginForm');
+const singupForm = document.getElementById('singupForm');
+const slider = document.querySelector('.toggle .slider');
